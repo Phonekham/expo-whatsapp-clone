a7e4dc71e881cd12aae18b550aeddf41db1c511c
Phonekham <30440409+Phonekham@users.noreply.github.com>
2020-10-14 11:09:39 +0700

    chatScreen with header

--- components/ChatListItem/Index.tsx
+++ components/ChatListItem/Index.tsx
@@ -1,6 +1,7 @@
 import moment from "moment";
 import React from "react";
-import { View, Text, Image } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { View, Text, Image, TouchableWithoutFeedback } from "react-native";
 
 import { ChatRoom } from "../../types";
 import styles from "./style";
@@ -12,19 +13,30 @@ export type ChatListItemProps = {
 const ChatListItem = (props: ChatListItemProps) => {
   const { chatRoom } = props;
   const user = chatRoom.users[1];
+
+  const navigation = useNavigation();
+
+  const onClick = () => {
+    navigation.navigate("ChatRoom", { id: chatRoom.id, name: user.name });
+  };
+
   return (
-    <View style={styles.container}>
-      <View style={styles.leftContainer}>
-        <Image source={{ uri: user.imageUri }} style={styles.avatar} />
-        <View style={styles.midContainer}>
-          <Text style={styles.username}>{user.name}</Text>
-          <Text style={styles.lastMessage}>{chatRoom.lastMessage.content}</Text>
+    <TouchableWithoutFeedback onPress={onClick}>
+      <View style={styles.container}>
+        <View style={styles.leftContainer}>
+          <Image source={{ uri: user.imageUri }} style={styles.avatar} />
+          <View style={styles.midContainer}>
+            <Text style={styles.username}>{user.name}</Text>
+            <Text style={styles.lastMessage}>
+              {chatRoom.lastMessage.content}
+            </Text>
+          </View>
+          <Text numberOfLines={2} style={styles.time}>
+            {moment(chatRoom.lastMessage.createdAt).format("DD/MM/YYYY")}
+          </Text>
         </View>
-        <Text numberOfLines={2} style={styles.time}>
-          {moment(chatRoom.lastMessage.createdAt).format("DD/MM/YYYY")}
-        </Text>
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 };
 
---
+++ screens/ChatRoomScreen.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { useRoute } from "@react-navigation/native";
+
+const ChatRoomScreen = () => {
+  const route = useRoute();
+
+  return (
+    <View>
+      <Text>chatroom</Text>
+    </View>
+  );
+};
+
+export default ChatRoomScreen;

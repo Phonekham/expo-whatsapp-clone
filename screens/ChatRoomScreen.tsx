4593e1a3ce8b733ad9dfc85d996e2dbd2902dfbd
Phonekham <30440409+Phonekham@users.noreply.github.com>
2020-10-14 12:47:58 +0700

    display message in chat

--- screens/ChatRoomScreen.tsx
+++ screens/ChatRoomScreen.tsx
@@ -1,14 +1,21 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, FlatList, ImageBackground } from "react-native";
 import { useRoute } from "@react-navigation/native";
+
+import ChatMessage from "../components/ChatMessage/Index";
+import chatRoomData from "../data/Chats";
+import BG from "../assets/images/BG.png";
 
 const ChatRoomScreen = () => {
   const route = useRoute();
 
   return (
-    <View>
-      <Text>chatroom</Text>
-    </View>
+    <ImageBackground source={BG} style={{ width: "100%", height: "100%" }}>
+      <FlatList
+        data={chatRoomData.messages}
+        renderItem={({ item }) => <ChatMessage message={item} />}
+      />
+    </ImageBackground>
   );
 };
 

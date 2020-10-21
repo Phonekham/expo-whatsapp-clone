15c29e043e465a1d533a2d13f7788f176154200f
Phonekham <30440409+Phonekham@users.noreply.github.com>
2020-10-21 11:26:14 +0700

    amplify add auth

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
+import { withAuthenticator } from "aws-amplify-react-native";
 import Amplify from "aws-amplify";
 import config from "./aws-exports";
 Amplify.configure(config);
@@ -10,7 +11,7 @@ import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation";
 
-export default function App() {
+function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
@@ -25,3 +26,5 @@ export default function App() {
     );
   }
 }
+
+export default withAuthenticator(App);
